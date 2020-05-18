e19331cb9fd46fb7b77cdfc30fa1ad416d8f68a8
Chris Spoehr <chris.spoehr@asynchrony.com>
2020-05-17 20:53:53 -0500

    Check for XCode CLI tools before proceeding with system steps

--- scripts/system.sh
+++ scripts/system.sh
@@ -3,6 +3,19 @@
 # DESCRIPTION
 # Installs OS X system software.
 
+echo "[SYSTEM] Checking for the XCode CLI tools"
+
+xcode-select -p 2> /dev/null
+
+if [ $? -eq 2 ]; then
+    echo "[SYSTEM] ERROR: XCode CLI tools not found. Installation has been requested." >/dev/stderr
+    echo "[SYSTEM] ERROR: Follow UI prompts to install CLI tools. Rerun this script when completed" >/dev/stderr
+    xcode-select --install
+    exit 2
+else
+    echo "[SYSTEM] XCode CLI tools found; proceeding with setup"
+fi
+
 if ! command -v brew >/dev/null; then
   echo "[SYSTEM] Install Homebrew"
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
